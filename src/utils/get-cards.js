11a1f7c2aa9fba2fe0e6126a9e83d56efb7b5262
Kelli Blalock <kelli.blalock2@gmail.com>
2019-04-06 03:08:53 -0700

    Add set name to cards

--- src/utils/get-cards.js
+++ src/utils/get-cards.js
@@ -13,10 +13,27 @@ const dataDir = path.join(__dirname, '../../data')
 
  // Get card data from HearthstoneJSON API
 axios.get(cardUrl).then((response) => {
-  saveCardData(JSON.stringify(response.data))
+  const cardsWithSets = addSetsToCards(response.data)
+  saveCardData(JSON.stringify(cardsWithSets))
 })
 
-// save cards data to data/cards.json
+// Add set name to cards based on the set code returned by the API
+const addSetsToCards = (cards) => {
+  let setsFilePath
+  let setsData
+  try {
+    setsFilePath = path.join(__dirname, '../../data/card-sets.json')
+    setsData = JSON.parse(fs.readFileSync(setsFilePath, 'utf-8'))
+  } catch {
+    console.error('Error obtaining set data. Please fix and restart server.')
+  }
+  cards.forEach(card => {
+    const set = setsData.filter(set => set.code === card.set)
+    card.setName = set[0].name
+  })
+}
+
+// Save cards data to data/cards.json
 const saveCardData = (cards) => {
   fs.writeFile(`${dataDir}/cards.json`, cards, (err) => {
     if (err) {
